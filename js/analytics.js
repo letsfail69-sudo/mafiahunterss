a95e6c7c0bb2bdeb45196443f62796d7f1ec6bac
letsfail69-sudo <letsfail69@gmail.com>
2026-07-08 12:50:31 +0200

    analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,8 +1,4 @@
 // Google Analytics 4
-const gaScript = document.createElement("script");
-gaScript.async = true;
-gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-BD4VKYXBTL";
-document.head.appendChild(gaScript);
 
 window.dataLayer = window.dataLayer || [];
 
@@ -10,5 +6,16 @@ function gtag() {
     dataLayer.push(arguments);
 }
 
-gtag("js", new Date());
-gtag("config", "G-BD4VKYXBTL");
+// Načtení Google Analytics
+const gaScript = document.createElement("script");
+gaScript.async = true;
+gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-BD4VKYXBTL";
+
+gaScript.onload = () => {
+    gtag("js", new Date());
+    gtag("config", "G-BD4VKYXBTL", {
+        anonymize_ip: true
+    });
+};
+
+document.head.appendChild(gaScript);
