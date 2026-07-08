c8a8857270b6ad749fdd03425c934c4618e212c8
letsfail69-sudo <letsfail69@gmail.com>
2026-07-08 12:34:46 +0200

    script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/* GOOGLE ANALYTICS */
+const gaScript = document.createElement("script");
+gaScript.async = true;
+gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-BD4VKYXBTL";
+document.head.appendChild(gaScript);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+gtag("config", "G-BD4VKYXBTL");
+
+
 const btn = document.querySelector('.hamb');
 const menu = document.querySelector('.menu');
 
